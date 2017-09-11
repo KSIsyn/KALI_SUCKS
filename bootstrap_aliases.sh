c6f3b8a49ee14769d1ee55c2548f0b9d2197f2d0
Lord Syn <celticruins@gmail.com>
2017-09-11 15:57:33 -0400

    persistent alias function
    added function to create persistent aliases

--- bootstrap_aliases.sh
+++ bootstrap_aliases.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
 
+##########################################
+# Temp function to create persistiant alias 
+##########################################
+
+function permalias () 
+{ 
+	alias "$*";
+	echo alias "$*" >> ~/.bash_aliases
+ fi
+}
+
+##########################################
+#quick change to parent directory
+##########################################
+permalias ..="cd .."
