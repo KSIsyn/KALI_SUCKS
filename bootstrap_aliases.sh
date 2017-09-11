01bb879e8b213ae717df76698fc12cde20af4012
Lord Syn <celticruins@gmail.com>
2017-09-11 17:30:32 -0400

    style updates

--- bootstrap_aliases.sh
+++ bootstrap_aliases.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 ##########################################
 # note to check for existing aliases script must be run with bash -i 
@@ -18,40 +18,38 @@ source ~/.bash_aliases
 
 function permalias () 
 { 
-	
- name="$(echo "$*" | cut -d "=" -f 1)"	
- if alias | grep -q $(echo "$*" | cut -d "=" -f 1) > /dev/null 
-	then
+	name="$(echo $* | cut -d '=' -f 1)"	
+	if alias | grep -q $(echo $* | cut -d '=' -f 1) > /dev/null; then
 		echo "Alias for $name already exists. Skipping to next alias"
-	else
+		else
 		echo "Adding new alias $* "
 		alias "$*";
 		echo alias "$*" >> ~/.bash_aliases
- fi
+ 	fi
 }
 
 ##########################################
 #quick change to parent directory
 ##########################################
-permalias ..="cd .."
+permalias ..='cd ..'
 
 ##########################################
 #list all items
 ##########################################
-permalias lh="ls -lhA --color=auto"
+permalias lh='ls -lhA --color=auto'
 
 ##########################################
 #list all items and pass to less for large directory listings
 ##########################################
-permalias lsl="ls -lhFA | less"
+permalias lsl='ls -lhFA | less'
 
 ##########################################
 #list all items recurrsivly and pipe to less
 ##########################################
-permalias lr="ls -lhAR --color=auto | less"
+permalias lr='ls -lhAR --color=auto | less'
 
 ##########################################
 #get systeminfo details
 ##########################################
-permalias ver="uname -srv"
-permalias sysinfo="uname -a"
+permalias ver='uname -srv'
+permalias sysinfo='uname -a'
