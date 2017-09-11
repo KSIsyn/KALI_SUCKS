8c04d91b297c556e7e1dba9cca52a4e4c012369f
Lord Syn <celticruins@gmail.com>
2017-09-11 16:00:22 -0400

    update function permalias()
    updated permalias() to prevent duplicate entries for an alias

--- bootstrap_aliases.sh
+++ bootstrap_aliases.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
 
 ##########################################
+# note to check for existing aliases script must be run with bash -i 
+##########################################
+
+##########################################
+# source and expand current aliases
+##########################################
+
+shopt -s expand_aliases
+source ~/.bashrc
+source ~/.bash_aliases
+
+##########################################
 # Temp function to create persistiant alias 
 ##########################################
 
 function permalias () 
 { 
-	alias "$*";
-	echo alias "$*" >> ~/.bash_aliases
+	
+ name="$(echo "$*" | cut -d "=" -f 1)"	
+ if alias | grep -q $(echo "$*" | cut -d "=" -f 1) > /dev/null 
+	then
+		echo "Alias for $name already exists. Skipping to next alias"
+	else
+		echo "Adding new alias $* "
+		alias "$*";
+		echo alias "$*" >> ~/.bash_aliases
  fi
 }
 
